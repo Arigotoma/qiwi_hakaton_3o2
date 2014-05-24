deed2a809e50777884b65f32d58f7216aed384a3
Arigotoma <miralexander@gmail.com>
2014-05-25 00:01:02 +0700

    3

--- core.js
+++ core.js
@@ -12,12 +12,50 @@ if (!Date.now) {
     Date.now = function() { return new Date().getTime(); };
 }
 
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+var DAMAGE_TYPE_FIRE = 0,
+    DAMAGE_TYPE_HOLE = 1;
+
+function Damage (dtype) {
+    this.type = dtype;
+    this.reparePersent = 100;
+    this.time = Date.now();
+}
+
 function Block () {
     this.energy = 0;
     this.people = 0;
+    this.repareTime = 10000;
     this._lastUpdateTime = Date.now();
 
-    this.calculate = function () {};
+    this.damages = [];
+
+    this.addDamage = function (damage) {
+        this.damages.push(damage);
+    };
+
+    this.getDamageByType = function (dtype) {
+        for (var i = 0; i < this.damages.length; i++) {
+            if (this.damages[i].type === dtype) {
+                return this.damages[i];
+            }
+        }
+        return undefined;
+    };
+
+    this.calculate = function () {
+        var d = Date.now() - this._lastUpdateTime;
+        if (this.people && this.damages.length) {
+            this.damages[0].reparePersent -= d*100/this.repareTime;
+            if (this.damages[0].reparePersent <= 0) {
+                this.damages.shift();
+            }
+        }
+    };
 }
 
 function Armament () {
@@ -25,6 +63,8 @@ function Armament () {
     this._reloadTime = 5000;
 
     this.calculate = function () {
+        this.prototype.calculate.call(this);
+
         if (this.reloadPersent < 100) {
             this.reloadPersent += (Date.now() - this._lastUpdateTime)*100/this._reloadTime;
             if (this.reloadPersent > 100) {
@@ -39,8 +79,34 @@ function Armament () {
         this.reloadPersent = 0;
     }
 }
-Armament.prototype = new Block()
+Armament.prototype = new Block();
 //extend(Armament, Block);
+
+function Oxygen () {
+    this.oxygen = 100;
+    this.lossPerSecond = 0.5;
+    this.damagedBlocks = 0;
+    this.damagedBlocksLossPerSecond = 0.5;
+
+    this.calculate = function () {
+        var d = Date.now() - this._lastUpdateTime;
+
+        this.oxygen -= d*this.lossPerSecond/1000;
+        this.oxygen -= d*this.damagedBlocksLossPerSecond*this.damagedBlocks/1000;
+
+        this.oxygen += d*this.lossPerSecond/1000;
+
+        if (this.oxygen > 100) {
+            this.oxygen = 100;
+        }
+        if (this.oxygen < 0) {
+            this.oxygen = 0;
+        }
+
+        this._lastUpdateTime = Date.now();
+    }
+}
+Oxygen.prototype = new Block();
 
 
 game.controller('GameCtrl', function ($scope) {
@@ -48,46 +114,78 @@ game.controller('GameCtrl', function ($scope) {
     $scope.BLOCK_ARMAMENT = 0;
     $scope.BLOCK_SHIELD = 1;
     $scope.BLOCK_ENGINE = 2;
+    $scope.BLOCK_OXYGEN = 3;
 
-    $scope.maxEnergy = 5;
-    $scope.lastEnergy = 5;
-    $scope.live = 100;
-    $scope.blocks = [new Armament(), new Block(), new Block(), new Block()];
+    $scope.player = {
+        live: 100,
+        oxygen: 100,
+        maxEnergy: 5,
+        lastEnergy: 5,
+        blocks: [new Armament(), new Block(), new Block(), new Oxygen()]
+    };
 
-    $scope.enemyLive = 100;
-    $scope.enemyBlocks = [new Armament(), new Block(), new Block(), new Block()];
+    $scope.enemy = {
+        live: 100,
+        oxygen: 100,
+        maxEnergy: 5,
+        lastEnergy: 5,
+        blocks: [new Armament(), new Block(), new Block(), new Oxygen()]
+    };
 
 
     $scope.init = function () {
         $scope.createEnemy();
-    }
+    };
 
     $scope.createEnemy = function () {
-        for (var i = 0; i < $scope.enemyBlocks.length; i++) {
-            $scope.enemyBlocks[i].energy = 1;
+        for (var i = 0; i < $scope.enemy.blocks.length; i++) {
+            $scope.enemy.blocks[i].energy = 1;
         }
     };
 
     $scope.changeEnergy = function (block, value) {
-        $scope.blocks[block].energy += value;
-        $scope.lastEnergy -= value;
+        $scope.player.blocks[block].energy += value;
+        $scope.player.lastEnergy -= value;
     };
 
 
     $scope.fire = function () {
-        $scope.blocks[$scope.BLOCK_ARMAMENT].fire();
+        //$scope.player.live -= 1;
+        $scope._fire($scope.player, $scope.enemy, getRandomInt(0, 3));
+
+    };
+
+    $scope._fire = function (attacker, victim, block_id) {
+        var damage = attacker.blocks[$scope.BLOCK_ARMAMENT].energy - victim.blocks[$scope.BLOCK_SHIELD].energy;
+        if (damage < 0 ) {
+            damage = 0;
+        }
+        attacker.blocks[$scope.BLOCK_ARMAMENT].fire();
+        victim.live += victim.blocks[$scope.BLOCK_SHIELD].energy - attacker.blocks[$scope.BLOCK_ARMAMENT].energy;
+
+        if (Math.random() < 0.2) {
+            attacker.blocks[block_id].addDamage(new Damage(getRandomInt(0, 1)));
+        }
     };
 
     $scope.update = function () {
         var i, block;
-        for (i = 0; i < $scope.blocks.length; i++) {
-            block = $scope.blocks[i];
+        for (i = 0; i < $scope.player.blocks.length; i++) {
+            block = $scope.player.blocks[i];
             block.calculate();
         }
 
-        for (i = 0; i < $scope.enemyBlocks.length; i++) {
-            block = $scope.enemyBlocks[i];
+        for (i = 0; i < $scope.enemy.blocks.length; i++) {
+            block = $scope.enemy.blocks[i];
             block.calculate();
+        }
+
+        $scope.enemyAttack();
+    };
+
+    $scope.enemyAttack = function () {
+        if ($scope.enemy.blocks[$scope.BLOCK_ARMAMENT].reloadPersent >= 100) {
+            $scope._fire($scope.enemy, $scope.player, getRandomInt(0, 3));
         }
     };
 
